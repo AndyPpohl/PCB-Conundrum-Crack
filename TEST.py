3e333ecff4635d76755957784e024b45fe43b960
unknown <rebeiroaloysius15@gmail.com>
2024-10-14 18:17:21 -0400

    thrown changes

---
+++ TEST.py
@@ -0,0 +1,11 @@
+# write a function to add two numbers and return the sum
+
+def add(a, b):
+    return a + b
+
+# write a function to subtract two numbers and return the difference
+
+def subtract(a, b):
+    return a - b
+
+# write a function to multiply two numbers and return the product
